1b910b25d9916e4b9a274b67d9473798c0b57d60
khanbeb <s.munnakhan@gmail.com>
2020-08-13 01:31:00 +0600

    true vs false

---
+++ trueandfalse.js
@@ -0,0 +1,17 @@
+// const age = -4;
+
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+const name = "Sajal";
+
+if(name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
