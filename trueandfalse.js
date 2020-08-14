4433ca0aa6d43237a8fa516925dfa4d38997beef
khanbeb <s.munnakhan@gmail.com>
2020-08-15 01:30:44 +0600

    complete all information

--- trueandfalse.js
+++ trueandfalse.js
@@ -7,7 +7,20 @@
 //     console.log("condition is false");
 // }
 
-const name = "Sajal";
+// const name = "Sajal";
+
+// below all are falsy//
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//below all are Truthy//
+//'0'
+// ' '
+
+let name
 
 if(name.length > 0){
     console.log("condition is true");
